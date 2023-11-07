c0f2ee1d3f3770c3d03067ddca6e7f24d8f13beb
Alex M. Maldonado <aalexmmaldonado@gmail.com>
2023-11-07 15:47:57 -0500

    Add script to unify residue ids

--- metalflare/__init__.py
+++ metalflare/__init__.py
@@ -1,8 +1,8 @@
 __version__ = "0.0.0"
 
-from ast import literal_eval
 import os
 import sys
+from ast import literal_eval
 
 from loguru import logger
 
--- metalflare/pdb/change/numbering.py
+++ metalflare/pdb/change/numbering.py
@@ -1,57 +1,133 @@
 """Standardizes residue ID numbering"""
+import argparse
+import os
 import re
+from collections.abc import Iterable
+
+from loguru import logger
 
 from ..utils import parse_resid, replace_in_pdb_line
 
 
 def assign_resid(
-    line: str, current_resid: int | None, current_original_resid: str
-) -> int:
+    line: str, current_resid: str | None, prev_original_resid: str
+) -> tuple[str, str]:
     r"""Determines residue ID based on a consistent numbering scheme.
 
     Args:
         line: Line that we are determining the residue ID to have.
         current_resid: Current residue ID that we are using.
-        current_original_resid: Original residue ID from the PDB file that we are grouping together.
+        prev_original_resid: Previous residue ID from the PDB file that we are
+            grouping together.
 
     Returns:
         Assigned residue ID for this line.
+
+        Next `prev_original_resid`.
     """
-    line_resid = parse_resid(line)
+    next_original_resid = parse_resid(line).strip()
+    logger.trace("Parsed residue ID from line: {}", next_original_resid)
 
     # We have our first residue.
     if current_resid is None:
-        try:
-            return int(line_resid)
-        except ValueError:
-            resid = re.sub("[^0-9]", "", str(line_resid))
-            if len(str(resid)) == 0:
-                resid = "1"
-            return int(resid)
-
-    # If the line's residue id is the same as the current original, then we should
-    # group this atom with the previous one.
-    if line_resid == current_original_resid:
-        return current_resid
-
-    # We have the next residue.
-    return current_resid + 1
+        logger.debug("Current residue ID is None; must be our first atom.")
+        assigned_resid = re.sub("[^0-9]", "", str(next_original_resid))
+        if len(str(assigned_resid)) == 0:
+            assigned_resid = "1"
+        logger.trace("Assigning residue ID: {}", assign_resid)
+    else:
+        # If the line's residue id is the same as the current original, then we should
+        # group this atom with the previous one.
+        assigned_resid = current_resid
+        if next_original_resid != prev_original_resid:
+            logger.debug("Parsed residue ID is not the same as previous.")
+            logger.trace("Previous residue ID: {}", assigned_resid)
+            assigned_resid = str(int(assigned_resid) + 1)
+            logger.trace("Next residue ID: {}", assigned_resid)
+    return assigned_resid, next_original_resid
 
 
 def unify_resid(
-    line: str, current_resid: int | None, current_original_resid: str
-) -> str:
+    line: str, current_resid: str | None, prev_original_resid: str
+) -> tuple[str, str, str]:
     r"""Unify residue ID in the PDB line based on previous ones.
 
     Args:
         line: Line that we are modifying.
         current_resid: Current residue ID that we are using.
-        current_original_resid: Original residue ID from the PDB file that we are
-            grouping together.
+        prev_original_resid: Original residue ID from previous line in the PDB file.
 
     Returns:
-        Line with new residue ID.
+        PDB line with the new residue ID.
+
+        Residue ID we are assigning this atom.
+
+        The original residue ID from `line` (i.e., the next `prev_original_resid`).
     """
-    resid = assign_resid(line, current_resid, current_original_resid)
-    new_line = replace_in_pdb_line(line, current_original_resid, str(resid), 24, 27)
-    return new_line
+    assigned_resid, next_original_resid = assign_resid(
+        line, current_resid, prev_original_resid
+    )
+    new_line = replace_in_pdb_line(
+        line, next_original_resid, assigned_resid.rjust(4) + " ", 22, 27
+    )
+    return new_line, assigned_resid, next_original_resid
+
+
+def run_unify_resids(pdb_path: str, output_path: str | None) -> Iterable[str]:
+    r"""Unify residue ID in the PDB lines.
+
+    Args:
+        pdb_path: Path to PDB file.
+        output_path: Path to save new PDB file. If `None`, then no file is written.
+
+    Returns:
+        PDB file lines.
+    """
+    logger.info("Unify residue IDs from {}", os.path.abspath(pdb_path))
+    with open(pdb_path, "r", encoding="utf-8") as f:
+        pdb_lines: Iterable[str] = f.readlines()
+
+    parse_structure = False
+    for i, line in enumerate(pdb_lines):
+        logger.debug("Processing line number: {}", i)
+        if line.startswith("TER"):
+            logger.debug("Encountered 'TER'. Setting parse_structure to False.")
+            parse_structure = False
+            continue
+
+        if line.startswith(("ATOM", "HETATM")):
+            # Activate coordinate parsing on first instance of ATOM or HETATM
+            if not parse_structure:
+                parse_structure = True
+                current_resid = None
+                current_original_resid = str(parse_resid(line))
+
+            pdb_lines[i], current_resid, current_original_resid = unify_resid(
+                line, current_resid, current_original_resid
+            )
+
+    if output_path is not None:
+        logger.info("Writing PDB file to {}", os.path.abspath(output_path))
+        with open(output_path, "w+", encoding="utf-8") as f:
+            f.writelines(pdb_lines)
+
+    return pdb_lines
+
+
+def cli_unify_resids() -> None:
+    r"""Command-line interface for unifying residue IDs in PDB files."""
+    parser = argparse.ArgumentParser(description="Unify residue IDs in PDB")
+    parser.add_argument(
+        "pdb_path",
+        type=str,
+        nargs="?",
+        help="Path to PDB file",
+    )
+    parser.add_argument(
+        "--output",
+        type=str,
+        nargs="?",
+        help="Path to new PDB file",
+    )
+    args = parser.parse_args()
+    run_unify_resids(args.pdb_path, args.output)
