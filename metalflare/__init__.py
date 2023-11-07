c777190f183c9b6779eb65717878aa2be9b8fa1e
Alex M. Maldonado <aalexmmaldonado@gmail.com>
2023-11-07 11:26:08 -0500

    Add loguru

--- metalflare/__init__.py
+++ metalflare/__init__.py
@@ -1,1 +1,33 @@
 __version__ = "0.0.0"
+
+import os
+import sys
+
+from loguru import logger
+
+logger.disable("metalflare")
+
+
+def enable_logging(level: int, file_path: str | None = None) -> None:
+    r"""Enable logging.
+
+    Args:
+        level: Requested log level: `10` is debug, `20` is info.
+        file_path: Also write logs to files here.
+    """
+    config = {"handlers": [{"sink": sys.stdout, "level": level}]}
+    if isinstance(file_path, str):
+        config["handlers"].append(
+            {"sink": file_path, "level": level, "serialize": True}
+        )
+    print(config)
+    # https://loguru.readthedocs.io/en/stable/api/logger.html#loguru._logger.Logger.configure
+    logger.configure(**config)
+
+    logger.enable("metalflare")
+
+
+if os.environ.get("METALFLARE_LOG", 0):
+    level = int(os.environ.get("METALFLARE_LOG_LEVEL", 20))
+    file_path = os.environ.get("METALFLARE_LOG_FILE_PATH", None)
+    enable_logging(level, file_path)
