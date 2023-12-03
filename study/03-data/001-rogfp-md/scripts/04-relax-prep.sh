8ad94b823c2126a3d782b5a61a647c1d843a8f1c
Alex M. Maldonado <aalexmmaldonado@gmail.com>
2023-12-02 23:49:09 -0500

    Prepare relax files

--- study/03-data/001-rogfp-md/scripts/04-relax-prep.sh
+++ study/03-data/001-rogfp-md/scripts/04-relax-prep.sh
@@ -11,8 +11,8 @@ export METALFLARE_LOG_LEVEL=10
 export METALFLARE_LOG_FILE_PATH="04-relax-prep.log"
 
 SIMULATIONS_DIR="../simulations"
-COORD_PATH="$SIMULATIONS_DIR/02-amber-prep/mol.inpcrd"
-TOPO_PATH="$SIMULATIONS_DIR/02-amber-prep/mol.prmtop"
+COORD_PATH="$SIMULATIONS_DIR/02-prep/mol.inpcrd"
+TOPO_PATH="$SIMULATIONS_DIR/02-prep/mol.prmtop"
 
 SAVE_DIR="$SIMULATIONS_DIR/04-relax"
 INPUT_DIR="$SAVE_DIR/inputs"
