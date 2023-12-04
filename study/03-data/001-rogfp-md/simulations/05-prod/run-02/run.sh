8fa8f9e7e15e1d82ae7ebeac34bdd2d7ca0ca9c0
Alex M. Maldonado <aalexmmaldonado@gmail.com>
2023-12-04 00:10:50 -0500

    Run prod prepare

---
+++ study/03-data/001-rogfp-md/simulations/05-prod/run-02/run.sh
@@ -0,0 +1,4 @@
+
+echo 'Starting 08_prod_npt'
+date
+pmemd.cuda -O -i $SLURM_SUBMIT_DIR/inputs/08_prod_npt.in -o $SLURM_SUBMIT_DIR/outputs/08_prod_npt.out -c $SLURM_SUBMIT_DIR/../../04-relax/run-02/outputs/07_relax_npt.rst -p $SLURM_SUBMIT_DIR/inputs/mol.prmtop -r $SLURM_SUBMIT_DIR/outputs/08_prod_npt.rst -x $SLURM_SUBMIT_DIR/outputs/08_prod_npt.nc -ref $SLURM_SUBMIT_DIR/inputs/mol.inpcrd -inf $SLURM_SUBMIT_DIR/outputs/08_prod_npt.mdinfo
