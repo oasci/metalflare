9c3c5a9776903b7136feac3fb2ab689465376651
Alex Maldonado <alex@oasci.org>
2023-12-06 17:57:09 -0500

    Update prep script

--- study/data/001-rogfp-md/scripts/03-min-prep.sh
+++ study/data/001-rogfp-md/scripts/03-min-prep.sh
@@ -11,28 +11,33 @@ export METALFLARE_LOG_LEVEL=10
 export METALFLARE_LOG_FILE_PATH="03-min-prep.log"
 
 SIMULATIONS_DIR="../simulations"
+METHODS_DIR="../../../methods"
+YAML_DIR="$METHODS_DIR/04-amber-simulations"
+
 COORD_PATH="$SIMULATIONS_DIR/02-prep/mol.inpcrd"
 TOPO_PATH="$SIMULATIONS_DIR/02-prep/mol.prmtop"
+STAGE_DIR="$SIMULATIONS_DIR/03-min"
+INPUT_DIR="$STAGE_DIR/inputs"
+OUTPUT_DIR="$STAGE_DIR/outputs"
 
-SAVE_DIR="$SIMULATIONS_DIR/03-min"
-INPUT_DIR="$SAVE_DIR/inputs"
-OUTPUT_DIR="$SAVE_DIR/outputs"
-RUN_PATH="$SAVE_DIR/run.sh"
-SLURM_PATH="$SAVE_DIR/submit.slurm"
 JOB_NAME="metalflare/001/03-min"
+WRITE_DIR="$INPUT_DIR"
+RUN_PATH="$STAGE_DIR/run.sh"
+SLURM_PATH="$STAGE_DIR/submit.slurm"
+PREP_CLASS_STRING="metalflare.simulation.amber.run.AmberRunPrep"
 
 
 # Cleanup files from previous run
-rm -rf $SAVE_DIR
-mkdir -p $SAVE_DIR
+rm -rf $STAGE_DIR
+mkdir -p $STAGE_DIR
 rm -rf $INPUT_DIR
 mkdir -p $INPUT_DIR
 rm -rf $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR
 rm -f $METALFLARE_LOG_FILE_PATH
 
-./prep_sim.py $TOPO_PATH $COORD_PATH $INPUT_DIR $RUN_PATH $JOB_NAME $SLURM_PATH \
---yaml $SIMULATIONS_DIR/base.yml $SIMULATIONS_DIR/03-min.yml $SIMULATIONS_DIR/slurm.yml
+metalflare-prep-sims $JOB_NAME $WRITE_DIR $RUN_PATH $SLURM_PATH $PREP_CLASS_STRING \
+--yaml $YAML_DIR/03-min.yml $YAML_DIR/slurm.yml $YAML_DIR/base.yml
 
 cp $COORD_PATH $INPUT_DIR/mol.inpcrd
 cp $TOPO_PATH $INPUT_DIR/mol.prmtop
