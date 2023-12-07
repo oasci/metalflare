fdcfa2b12c4164aa84d34b191f4dd9ba00f752c4
Alex Maldonado <alex@oasci.org>
2023-12-06 19:22:07 -0500

    Update metalflare/001/04-relax

--- study/data/001-rogfp-md/scripts/04-relax-prep.sh
+++ study/data/001-rogfp-md/scripts/04-relax-prep.sh
@@ -11,6 +11,9 @@ export METALFLARE_LOG_LEVEL=10
 export METALFLARE_LOG_FILE_PATH="04-relax-prep.log"
 
 SIMULATIONS_DIR="../simulations"
+METHODS_DIR="../../../methods"
+YAML_DIR="$METHODS_DIR/04-amber-simulations"
+
 COORD_PATH="$SIMULATIONS_DIR/02-prep/mol.inpcrd"
 TOPO_PATH="$SIMULATIONS_DIR/02-prep/mol.prmtop"
 
@@ -20,25 +23,28 @@ for ((i=1; i<=$REPLICATES; i++)); do
     suffix=$(printf "%02d" "$i")
 
     RUN_NAME="run-$suffix"
-    SAVE_DIR="$SIMULATIONS_DIR/04-relax/$RUN_NAME"
-    INPUT_DIR="$SAVE_DIR/inputs"
-    OUTPUT_DIR="$SAVE_DIR/outputs"
-    RUN_PATH="$SAVE_DIR/run.sh"
-    SLURM_PATH="$SAVE_DIR/submit.slurm"
-    JOB_NAME="metalflare/001/04-relax/$RUN_NAME"
 
+    STAGE_DIR="$SIMULATIONS_DIR/04-relax/$RUN_NAME"
+    INPUT_DIR="$STAGE_DIR/inputs"
+    OUTPUT_DIR="$STAGE_DIR/outputs"
+
+    JOB_NAME="metalflare/001/04-relax/$RUN_NAME"
+    WRITE_DIR="$INPUT_DIR"
+    RUN_PATH="$STAGE_DIR/run.sh"
+    SLURM_PATH="$STAGE_DIR/submit.slurm"
+    PREP_CLASS_STRING="metalflare.simulation.amber.run.AmberRunPrep"
 
     # Cleanup files from previous run
-    rm -rf $SAVE_DIR
-    mkdir -p $SAVE_DIR
+    rm -rf $STAGE_DIR
+    mkdir -p $STAGE_DIR
     rm -rf $INPUT_DIR
     mkdir -p $INPUT_DIR
     rm -rf $OUTPUT_DIR
     mkdir -p $OUTPUT_DIR
     rm -f $METALFLARE_LOG_FILE_PATH
 
-    ./prep_sim.py $TOPO_PATH $COORD_PATH $INPUT_DIR $RUN_PATH $JOB_NAME $SLURM_PATH \
-    --yaml $SIMULATIONS_DIR/base.yml $SIMULATIONS_DIR/slurm.yml $SIMULATIONS_DIR/04-relax.yml
+    metalflare-prep-sims $JOB_NAME $WRITE_DIR $RUN_PATH $SLURM_PATH $PREP_CLASS_STRING \
+    --yaml $YAML_DIR/04-relax.yml $YAML_DIR/slurm.yml $YAML_DIR/base.yml
 
     cp $COORD_PATH $INPUT_DIR/mol.inpcrd
     cp $TOPO_PATH $INPUT_DIR/mol.prmtop
