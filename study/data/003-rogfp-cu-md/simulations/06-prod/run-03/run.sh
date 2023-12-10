a99027d936efe6fdec46e6bf4495cf980f772246
Alex M. Maldonado <aalexmmaldonado@gmail.com>
2023-12-09 22:30:20 -0500

    Use original prmtop and inpcrd in 02-prep

--- study/data/003-rogfp-cu-md/simulations/06-prod/run-03/run.sh
+++ study/data/003-rogfp-cu-md/simulations/06-prod/run-03/run.sh
@@ -1,8 +1,8 @@
 
 echo 'Starting 09_prod_npt'
 date
-pmemd.cuda -O -i $SLURM_SUBMIT_DIR/inputs/09_prod_npt.in -o $SLURM_SUBMIT_DIR/outputs/09_prod_npt.out -c $SLURM_SUBMIT_DIR/../../05-prod/run-03/outputs/08_prod_npt.rst -p $SLURM_SUBMIT_DIR/inputs/mol.prmtop -r $SLURM_SUBMIT_DIR/outputs/09_prod_npt.rst -x $SLURM_SUBMIT_DIR/outputs/09_prod_npt.nc -ref $SLURM_SUBMIT_DIR/inputs/mol.inpcrd -inf $SLURM_SUBMIT_DIR/outputs/09_prod_npt.mdinfo
+pmemd.cuda -O -i $SLURM_SUBMIT_DIR/inputs/09_prod_npt.in -o $SLURM_SUBMIT_DIR/outputs/09_prod_npt.out -c $SLURM_SUBMIT_DIR/../../05-prod/run-03/outputs/08_prod_npt.rst -p $SLURM_SUBMIT_DIR/../../02-prep/mol.prmtop -r $SLURM_SUBMIT_DIR/outputs/09_prod_npt.rst -x $SLURM_SUBMIT_DIR/outputs/09_prod_npt.nc -ref $SLURM_SUBMIT_DIR/../../02-prep/mol.inpcrd -inf $SLURM_SUBMIT_DIR/outputs/09_prod_npt.mdinfo
 
 echo 'Starting 10_prod_npt'
 date
-pmemd.cuda -O -i $SLURM_SUBMIT_DIR/inputs/10_prod_npt.in -o $SLURM_SUBMIT_DIR/outputs/10_prod_npt.out -c $SLURM_SUBMIT_DIR/outputs/09_prod_npt.rst -p $SLURM_SUBMIT_DIR/inputs/mol.prmtop -r $SLURM_SUBMIT_DIR/outputs/10_prod_npt.rst -x $SLURM_SUBMIT_DIR/outputs/10_prod_npt.nc -ref $SLURM_SUBMIT_DIR/inputs/mol.inpcrd -inf $SLURM_SUBMIT_DIR/outputs/10_prod_npt.mdinfo
+pmemd.cuda -O -i $SLURM_SUBMIT_DIR/inputs/10_prod_npt.in -o $SLURM_SUBMIT_DIR/outputs/10_prod_npt.out -c $SLURM_SUBMIT_DIR/outputs/09_prod_npt.rst -p $SLURM_SUBMIT_DIR/../../02-prep/mol.prmtop -r $SLURM_SUBMIT_DIR/outputs/10_prod_npt.rst -x $SLURM_SUBMIT_DIR/outputs/10_prod_npt.nc -ref $SLURM_SUBMIT_DIR/../../02-prep/mol.inpcrd -inf $SLURM_SUBMIT_DIR/outputs/10_prod_npt.mdinfo
