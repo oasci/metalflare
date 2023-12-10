11c9540e1786f8b702d6a684c681c92822f8da82
Alex Maldonado <alex@oasci.org>
2023-12-09 19:26:20 -0500

    Fix writing pdb function

---
+++ study/data/001-rogfp-md/scripts/prod-to-pdbs.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+(
+# Ensure we are in the directory of this script.
+cd "$(dirname "$0")"
+
+SIMULATIONS_DIR="../simulations"
+REPLICATES=3
+
+for ((i=1; i<=$REPLICATES; i++)); do
+    suffix=$(printf "%02d" "$i")
+
+    RUN_NAME="run-$suffix"
+
+    STAGE_DIR="$SIMULATIONS_DIR/05-prod/$RUN_NAME"
+    OUTPUT_DIR="$STAGE_DIR/outputs"
+    COORD_PATH="$OUTPUT_DIR/08_prod_npt.nc"
+    TOPO_PATH="$SIMULATIONS_DIR/02-prep/mol.prmtop"
+
+    metalflare-pdb $OUTPUT_DIR/08_prod_npt.pdb --files $TOPO_PATH $COORD_PATH --stride 100 --select not resname WAT and not resname Cl- and not resname Na+
+done
+
+)
