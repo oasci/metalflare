56b39d74257de3240596f7f3895248ec2907face
Alex Maldonado <alex@oasci.org>
2023-12-09 20:36:19 -0500

    Only keep run-01 and align

--- study/data/001-rogfp-md/scripts/prod-to-pdbs.sh
+++ study/data/001-rogfp-md/scripts/prod-to-pdbs.sh
@@ -5,7 +5,7 @@
 cd "$(dirname "$0")"
 
 SIMULATIONS_DIR="../simulations"
-REPLICATES=3
+REPLICATES=1
 
 for ((i=1; i<=$REPLICATES; i++)); do
     suffix=$(printf "%02d" "$i")
@@ -17,7 +17,9 @@ for ((i=1; i<=$REPLICATES; i++)); do
     COORD_PATH="$OUTPUT_DIR/08_prod_npt.nc"
     TOPO_PATH="$SIMULATIONS_DIR/02-prep/mol.prmtop"
 
-    metalflare-pdb $OUTPUT_DIR/08_prod_npt.pdb --files $TOPO_PATH $COORD_PATH --stride 250 --select not resname WAT and not resname Cl- and not resname Na+
+    metalflare-pdb $OUTPUT_DIR/08_prod_npt_raw.pdb --files $TOPO_PATH $COORD_PATH --stride 250 --select not resname WAT and not resname Cl- and not resname Na+
+    metalflare-pdb-align $OUTPUT_DIR/08_prod_npt_raw.pdb $OUTPUT_DIR/08_prod_npt.pdb --select protein
+    rm $OUTPUT_DIR/08_prod_npt_raw.pdb
 done
 
 )
