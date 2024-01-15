730dce42a0d619b7bd30c762e7241645c2d5f0c6
Alex Maldonado <alex@oasci.org>
2024-01-14 20:32:57 -0500

    Compute rdfs of bound

--- study/figures/002-rogfp2-cro-rdf/cro-rdf-plot.py
+++ study/figures/002-rogfp2-cro-rdf/cro-rdf-plot.py
@@ -4,7 +4,6 @@ import os
 
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
@@ -12,25 +11,43 @@ if __name__ == "__main__":
     # Specify the paths to the trajectory and topology files
     base_dir = "../../"
 
-    for atom_type in ["C", "H", "O", "N", "S"]:
-        density_path = os.path.join(
+    for atom_type in ["H", "O", "N"]:
+        # bins
+        bins_path_unbound = os.path.join(
+            base_dir,
+            "analysis/001-rogfp-md/data/rdf/",
+            f"cro_{atom_type.lower()}_rdf_bins.npy",
+        )
+        bin_edges_unbound = np.load(bins_path_unbound)
+
+        bins_path_bound = os.path.join(
+            base_dir,
+            "analysis/003-rogfp-cu-md/data/rdf/",
+            f"cro_{atom_type.lower()}_rdf_bins.npy",
+        )
+        bin_edges_bound = np.load(bins_path_bound)
+
+        # rdf
+        density_path_unbound = os.path.join(
             base_dir,
             "analysis/001-rogfp-md/data/rdf/",
             f"cro_{atom_type.lower()}_rdf_density.npy",
         )
-        density = np.load(density_path)
+        density_unbound = np.load(density_path_unbound)
+        density_path_bound = os.path.join(
+            base_dir,
+            "analysis/003-rogfp-cu-md/data/rdf/",
+            f"cro_{atom_type.lower()}_rdf_density.npy",
+        )
+        density_bound = np.load(density_path_bound)
 
-        kwargs = {
-            "kde": True,
-            "stat": "density",
-            "fill": True,
-            "binrange": (0, 15),
-            "bins": 150,
-        }
-        sns.histplot(density, **kwargs)
+        fig, ax = plt.subplots(nrows=1, ncols=1)
+        ax.plot(bin_edges_unbound, density_unbound, label="Unbound", color="#1e2e79")
+        ax.plot(bin_edges_bound, density_bound, label="Bound", color="#f99752")
 
-        plt.xlabel("Distance [Å]")
-        plt.ylabel(f"g(O, {atom_type})")
+        ax.set_xlabel("Distance [Å]")
+        ax.set_ylabel("$g_{O" + atom_type + "}$")
 
+        plt.legend()
         plt.savefig(f"cro-rdf-{atom_type}.png")
         plt.clf()
