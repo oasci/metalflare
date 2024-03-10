d22fb3574ec3acc77407c2af254c3c6806a497e6
Alex Maldonado <alex@oasci.org>
2024-03-10 10:46:41 -0400

    Add all data scripts for 004

---
+++ study/analysis/004-rogfp-oxd-md/index.md
@@ -0,0 +1,4 @@
+# 004-rogfp-oxd-md
+
+Scripts here are focused on storing structural information from trajectory files into NumPy arrays.
+These arrays contain atom-to-atom distances and dihedral angles of residues around the chromophore.
---
+++ study/analysis/004-rogfp-oxd-md/scripts/ser203_h-asn144_o-dist.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import os
+
+import MDAnalysis as mda
+import numpy as np
+
+
+def generate_trajectory_paths(base_dir, run_range=(1, 4), prod_range=(8, 11)):
+    trajectory_paths = []
+    for run_i in range(*run_range):
+        trajectory_paths.extend(
+            [
+                os.path.join(
+                    base_dir,
+                    f"data/004-rogfp-oxd-md/simulations/05-prod/run-0{run_i}/outputs/{prod:02d}_prod_npt.nc",
+                )
+                for prod in range(*prod_range)
+            ]
+        )
+    return trajectory_paths
+
+
+def main():
+    base_dir = "/ihome/jdurrant/amm503/bgfs/oasci/metalflare/study"
+    trajectory_paths = generate_trajectory_paths(base_dir)
+
+    topology_path = os.path.join(
+        base_dir, "data/004-rogfp-oxd-md/simulations/02-prep/mol.prmtop"
+    )
+    atoms1_str = "resid 203 and name H"
+    atoms2_str = "resid 144 and name O"
+
+    data_dir = os.path.join(base_dir, "analysis/004-rogfp-oxd-md/data/struct-desc/")
+    os.makedirs(data_dir, exist_ok=True)
+
+    u = mda.Universe(topology_path, trajectory_paths)
+    n_frames = len(u.trajectory)
+
+    atoms_1 = u.select_atoms(atoms1_str)
+    atoms_2 = u.select_atoms(atoms2_str)
+
+    atoms_npy_path = os.path.join(data_dir, "ser203_h-asn144_o-dist.npy")
+    atoms_dist_array = np.full((n_frames,), np.nan, dtype=np.float64)
+
+    for i, ts in enumerate(u.trajectory):
+        dist = np.linalg.norm(atoms_1.positions - atoms_2.positions)
+        atoms_dist_array[i] = dist
+
+    np.save(atoms_npy_path, atoms_dist_array)
+
+    print(atoms_dist_array)
+
+
+if __name__ == "__main__":
+    main()
