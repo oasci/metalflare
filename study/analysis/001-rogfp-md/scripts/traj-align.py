ec2c8808e22456a498439cbb7694e05bfcaa8b1f
Alex Maldonado <alex@oasci.org>
2024-04-30 22:07:06 -0400

    Add traj align

---
+++ study/analysis/001-rogfp-md/scripts/traj-align.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import os
+
+import MDAnalysis as mda
+import numpy as np
+from MDAnalysis.analysis.align import AlignTraj
+
+
+def generate_trajectory_paths(base_dir, run_range=(1, 4), prod_range=(8, 11)):
+    trajectory_paths = []
+    for run_i in range(*run_range):
+        trajectory_paths.extend(
+            [
+                os.path.join(
+                    base_dir,
+                    f"data/001-rogfp-md/simulations/05-prod/run-0{run_i}/outputs/{prod:02d}_prod_npt.nc",
+                )
+                for prod in range(*prod_range)
+            ]
+        )
+    return [
+        path for path in trajectory_paths if "run-01/outputs/10_prod_npt" not in path
+    ]
+
+
+def main():
+    base_dir = "/ihome/jdurrant/amm503/ix/oasci/metalflare/study"
+    trajectory_paths = generate_trajectory_paths(base_dir)
+    topology_path = os.path.join(
+        base_dir, "data/001-rogfp-md/simulations/02-prep/mol.prmtop"
+    )
+
+    data_dir = os.path.join(base_dir, "analysis/001-rogfp-md/data/traj")
+    os.makedirs(data_dir, exist_ok=True)
+
+    u = mda.Universe(topology_path, trajectory_paths)
+
+    print("Aligning trajectory")
+    align_path = os.path.join(data_dir, "aligned_cro.nc")
+    alignment = AlignTraj(u, u, select="resname CRO", filename=align_path)
+    alignment.run()
+
+
+if __name__ == "__main__":
+    main()
