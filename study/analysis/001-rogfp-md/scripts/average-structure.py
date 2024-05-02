4c9144837fe5b754a48dee8ad76b588e0615aca9
Alex Maldonado <alex@oasci.org>
2024-05-02 12:42:15 -0400

    Compute average structure for 001

---
+++ study/analysis/001-rogfp-md/scripts/average-structure.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import os
+
+import MDAnalysis as mda
+import numpy as np
+from MDAnalysis.analysis import align
+
+
+def main():
+    base_dir = "/ihome/jdurrant/amm503/ix/oasci/metalflare/study"
+    trajectory_path = os.path.join(
+        base_dir, "analysis/001-rogfp-md/data/traj/aligned_cro.nc"
+    )
+    topology_path = os.path.join(
+        base_dir, "data/001-rogfp-md/simulations/02-prep/mol.prmtop"
+    )
+
+    data_dir = os.path.join(base_dir, "analysis/001-rogfp-md/data/structure")
+    os.makedirs(data_dir, exist_ok=True)
+
+    u = mda.Universe(topology_path, trajectory_path)
+    data_path = os.path.join(data_dir, "average-structure-cro.pdb")
+    align.AverageStructure(
+        u, filename=data_path, select="resname CRO", ref_frame=0
+    ).run()
+    data_path = os.path.join(data_dir, "average-structure.pdb")
+    align.AverageStructure(
+        u, filename=data_path, select="protein or resname CRO", ref_frame=0
+    ).run()
+
+
+if __name__ == "__main__":
+    main()
