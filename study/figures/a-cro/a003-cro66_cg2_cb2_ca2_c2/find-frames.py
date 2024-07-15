ad994a8f26c0577fcd02c4efe83ba572dbec2682
Alex Maldonado <alex@oasci.org>
2024-07-15 10:46:47 -0400

    Add a003 frames

---
+++ study/figures/a-cro/a003-cro66_cg2_cb2_ca2_c2/find-frames.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import os
+import json
+
+import numpy as np
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+
+if __name__ == "__main__":
+    base_dir = "../../../"
+
+    desc_to_get = {
+        "reduced": {
+            "path_data": os.path.join(
+                base_dir,
+                "analysis/005-rogfp-glh-md/data/struct-desc/cro65_cg2_cb2_ca2_c2-dihedral.npy",
+            ),
+            "values": [np.pi, 1.047, 0.0],
+        },
+        "oxidized": {
+            "path_data": os.path.join(
+                base_dir,
+                "analysis/007-rogfp-oxd-glh-md/data/struct-desc/cro65_cg2_cb2_ca2_c2-dihedral.npy",
+            ),
+            "values": [np.pi, 1.047, 0.0],
+        },
+        "cu": {
+            "path_data": os.path.join(
+                base_dir,
+                "analysis/006-rogfp-cu-glh-md/data/struct-desc/cro65_cg2_cb2_ca2_c2-dihedral.npy",
+            ),
+            "values": [np.pi, 1.047, 0.0],
+        },
+    }
+
+    json_data = {}
+    for key, info in desc_to_get.items():
+        json_data[key] = []
+        data = np.load(info["path_data"])
+        values = np.array(info["values"])
+        for value in values:
+            data_diff = np.abs(data - value)
+            arg_best = np.argmin(data_diff)
+            json_data[key].append(
+                {"value": str(data[arg_best]), "index": str(arg_best)}
+            )
+
+    path_out = "relevant-frames.json"
+    with open(path_out, "w", encoding="utf-8") as f:
+        json.dump(json_data, f, indent=4)
