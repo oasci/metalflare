5c501b510757a4f209c475d1ebf51d0219522be3
Alex Maldonado <alex@oasci.org>
2024-07-18 19:24:06 -0400

    Add cys202_c-ser203_n_ca_c-dihedral

---
+++ study/analysis/006-rogfp-cu-glh-md/scripts/cys202_c-ser203_n_ca_c-dihedral.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import os
+
+import MDAnalysis as mda
+import numpy as np
+
+
+def generate_trajectory_paths(base_dir, run_range=(1, 4), prod_range=(8, 11)):
+    trajectory_paths = []
+    for run_i in range(*run_range):
+        trajectory_paths.extend(
+            [
+                os.path.join(
+                    base_dir,
+                    f"data/006-rogfp-cu-glh-md/simulations/05-prod/run-0{run_i}/outputs/{prod:02d}_prod_npt.nc",
+                )
+                for prod in range(*prod_range)
+            ]
+        )
+    return trajectory_paths
+
+
+def compute_dihedralal_angle(coords):
+    # Given a set of coordinates, compute the dihedralal angle
+    # For example, you can use the dihedral function from MDAnalysis
+    return mda.lib.distances.calc_dihedrals(coords)
+
+
+def main():
+    base_dir = "/ihome/jdurrant/amm503/ix/oasci/metalflare/study"
+    trajectory_paths = generate_trajectory_paths(base_dir)
+
+    topology_path = os.path.join(
+        base_dir, "data/006-rogfp-cu-glh-md/simulations/02-prep/mol.prmtop"
+    )
+
+    data_dir = os.path.join(base_dir, "analysis/006-rogfp-cu-glh-md/data/struct-desc/")
+    os.makedirs(data_dir, exist_ok=True)
+
+    u = mda.Universe(topology_path, trajectory_paths)
+    n_frames = len(u.trajectory)
+
+    atoms = u.select_atoms("(resid 202 and name C) or (resid 203 and name N CA C)")
+
+    atoms_npy_path = os.path.join(data_dir, "cys202_c-ser203_n_ca_c-dihedral.npy")
+    atoms_dihedral_array = np.full((n_frames,), np.nan, dtype=np.float64)
+
+    for i, ts in enumerate(u.trajectory):
+        coords = atoms.positions
+        dihedral_angle = mda.lib.distances.calc_dihedrals(*coords)
+        atoms_dihedral_array[i] = dihedral_angle
+
+    np.save(atoms_npy_path, atoms_dihedral_array)
+
+    print(atoms_dihedral_array)
+
+
+if __name__ == "__main__":
+    main()
