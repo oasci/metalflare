235e4f5aea4d78657d7b3b930fc5a06be4daaa22
Alex M. Maldonado <aalexmmaldonado@gmail.com>
2024-08-04 14:34:09 -0400

    Fix analysis script

--- study/analysis/005-rogfp-glh-md/scripts/glu220_n_ca_cb_cg-dihedral.py
+++ study/analysis/005-rogfp-glh-md/scripts/glu220_n_ca_cb_cg-dihedral.py
@@ -1,4 +1,44 @@
-study/analysis/005-rogfp-glh-md/scripts/cys202_n_ca_cb_sg-dihedral.py
+#!/usr/bin/env python3
+
+import os
+
+import MDAnalysis as mda
+import numpy as np
+
+SIM_LABEL = os.path.dirname(os.path.abspath(__file__)).split("/")[-2]
+
+
+def generate_trajectory_paths(base_dir, run_range=(1, 4), prod_range=(8, 11)):
+    trajectory_paths = []
+    for run_i in range(*run_range):
+        trajectory_paths.extend(
+            [
+                os.path.join(
+                    base_dir,
+                    f"data/{SIM_LABEL}/simulations/05-prod/run-0{run_i}/outputs/{prod:02d}_prod_npt.nc",
+                )
+                for prod in range(*prod_range)
+            ]
+        )
+    return trajectory_paths
+
+
+def compute_dihedralal_angle(coords):
+    # Given a set of coordinates, compute the dihedralal angle
+    # For example, you can use the dihedral function from MDAnalysis
+    return mda.lib.distances.calc_dihedrals(coords)
+
+
+def main():
+    base_dir = "/ihome/jdurrant/amm503/ix/oasci/metalflare/study"
+    trajectory_paths = generate_trajectory_paths(base_dir)
+
+    topology_path = os.path.join(
+        base_dir, f"data/{SIM_LABEL}/simulations/02-prep/mol.prmtop"
+    )
+
+    data_dir = os.path.join(base_dir, f"analysis/{SIM_LABEL}/data/struct-desc/")
+    os.makedirs(data_dir, exist_ok=True)
 
     u = mda.Universe(topology_path, trajectory_paths)
     n_frames = len(u.trajectory)
